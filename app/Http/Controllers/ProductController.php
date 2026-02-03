0fab0b74ad729347f5582d63733d456f0e80e217
Shyam_Prasath <shyamprasath.priyadakumar@mindzen.co.in>
2026-02-03 12:27:09 +0530

    Product CRUD with Validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -10,9 +11,57 @@ class ProductController extends Controller
 
     public function index()
     {
+        $product = Product::all();
         return response()->json([
             'status' => true,
-            'message' => 'Hello World! Product API is working'
+            'message' => 'Product fetched successfully',
+            'data' => $product
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ]);
+        Product::create($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Product created successfully'
+        ]);
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return response()->json([
+            'status' => true,
+            'message' => 'Product fetched successfully',
+            'data' => $product
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Product updated successfully',
+            'data' => $product
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Product deleted successfully',
+            'data' => $product
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,8 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
