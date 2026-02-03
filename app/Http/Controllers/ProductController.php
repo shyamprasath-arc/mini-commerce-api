01dc3225ee05eebffaefab277f250b73356543ef
Shyam_Prasath <shyamprasath.priyadakumar@mindzen.co.in>
2026-02-03 11:52:32 +0530

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return response()->json([
+            'status' => true,
+            'message' => 'Hello World! Product API is working'
+        ]);
+    }
+}
